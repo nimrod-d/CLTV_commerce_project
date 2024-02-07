55c83c175f3545c2c1288faec12efd7ec524a298
nimrod-d <nimroddadush@gmail.com>
2024-02-07 16:59:11 +0100

    .

--- my_func.py
+++ my_func.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns 
+import statsmodels.api as sm
 from sklearn.preprocessing import MinMaxScaler, PowerTransformer, StandardScaler 
 from sklearn.linear_model import LinearRegression 
 from sklearn.model_selection import train_test_split
